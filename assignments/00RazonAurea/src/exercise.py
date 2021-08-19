d620f7045ea27f116694d709a78950ed2f4a3fe8
a01740729 <a01740729@itesm.mx>
2021-08-19 23:26:41 +0000

    resolviend ActClase 4.

--- assignments/00RazonAurea/src/exercise.py
+++ assignments/00RazonAurea/src/exercise.py
@@ -1,6 +1,16 @@
+import math
 def main():
     #escribe tu código abajo de esta línea
     pass
+    a = float(input('Dame un número: '))
+    b = int(input('¿Cuántos decimales? '))
+
+    varphi = (1 + math.sqrt (5)) / 2
+    ra = varphi * a
+
+    print("Número: "+str(a))
+    print("Decimales a mostrar: "+str(b))
+    print("Razón aurea: "+str(round(ra,b)))
 
 if __name__ == '__main__':
     main()
--- assignments/01CantidadLitrosPintura/src/exercise.py
+++ assignments/01CantidadLitrosPintura/src/exercise.py
@@ -1,6 +1,15 @@
+import math
 def main():
     #escribe tu código abajo de esta línea
     pass
+    a = float(input('Introduce el area a pintar en metros: '))
+    b = float(input('¿Cuantos metros puede pintar cada litro? '))
+
+    l = a / b
+
+    print("NArea a pintar en metros: "+str(a))
+    print("Rendimiento (m2/l): "+str(b))
+    print("Litros a comprar: "+str(math.ceil(l)))
 
 if __name__ == '__main__':
     main()
