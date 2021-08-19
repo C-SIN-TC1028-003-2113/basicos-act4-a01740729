92467e30e8f6313dc61cac7df803d9cff6acc5fb
a01740729 <a01740729@itesm.mx>
2021-08-19 23:35:22 +0000

    resolviendo actclase 4 ahora si

--- assignments/00RazonAurea/src/exercise.py
+++ assignments/00RazonAurea/src/exercise.py
@@ -2,14 +2,12 @@ import math
 def main():
     #escribe tu código abajo de esta línea
     pass
-    a = float(input('Dame un número: '))
-    b = int(input('¿Cuántos decimales? '))
+    a = float(input('Número: '))
+    b = int(input('Decimales a mostrar: '))
 
     varphi = (1 + math.sqrt (5)) / 2
     ra = varphi * a
 
-    print("Número: "+str(a))
-    print("Decimales a mostrar: "+str(b))
     print("Razón áurea: "+str(round(ra,b)))
 
 if __name__ == '__main__':
